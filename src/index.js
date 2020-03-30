6f4f39f2ec818c8e1b279de8ac5e965c8645c64c
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-03-30 11:48:10 +0300

    inital project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <h1>Hello world</h1>
+  )
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
