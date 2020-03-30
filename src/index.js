c11d114c05622ac3af2c963be57ec2b973d41d39
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2020-03-30 12:16:17 +0300

    learned use-state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
   return (
-    <h1>Hello world</h1>
+    <HookSwither/>
+  )
+}
+
+const HookSwither = () => {
+
+  const [ color, setColor ] = useState('white')
+  const [ fontSize, setfontSize ] = useState(14)
+
+
+  return (
+    <div style={{
+        padding: '10px', 
+        backgroundColor: color,
+        fontSize: `${fontSize}px`
+      }}>
+      Font
+      <button onClick={() => {setColor('black')}}>Dark</button>
+      <button onClick={() => {setColor('white')}}>Light</button>
+      <button 
+        onClick={() => {setfontSize((s) => s + 2)}}
+      >
+        +
+      </button>
+    </div>
   )
 }
 
